6e79b69f079b43d1a465277866571bd53026890d
Patrick Szmidt <pszmidt@gmail.com.(none)>
2023-04-03 16:05:35 +0200

    selection persistante des utilisations

--- app/controllers/utilisations_controller.rb
+++ app/controllers/utilisations_controller.rb
@@ -5,17 +5,25 @@ class UtilisationsController < ApplicationController
   # GET /utilisations or /utilisations.json
   def index
     if params[:tout] && params[:tout] == "1"
-      utilisations = Utilisation.all.order(created_at: :desc)
+      @utilisations = Utilisation.all
     else
-      utilisations = Utilisation.where(checked: false).order(created_at: :desc)
+      @utilisations = Utilisation.where(checked: false)
     end
 
-    if params[:start_date].present?
+    if params[:start_date].present? && params[:end_date].present?
       @start_date = Date.parse(params[:start_date])
       @end_date = Date.parse(params[:end_date])
-      @utilisations = utilisations.where(date: @start_date..@end_date).order(created_at: :desc)
-    else
-      @utilisations = utilisations
+      @utilisations = @utilisations.where(date: @start_date..@end_date)
+    end
+
+    if params[:tri] == "ressource"
+      @utilisations = @utilisations.order(ressource_id: :asc)
+    elsif params[:tri] == "chantier"
+      @utilisations = @utilisations.order(chantier_id: :asc)
+    elsif params[:tri] == "date de création"
+      @utilisations = @utilisations.order(created_at: :desc)
+    else params[:tri] == "date d'utilisation"
+      @utilisations = @utilisations.order(date: :desc)
     end
     
     if turbo_frame_request?
