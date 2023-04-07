1b2e5713e5b8f67e30cb8808124a2c723ecd94aa
Patrick Szmidt <pszmidt@gmail.com.(none)>
2023-04-07 16:30:33 +0200

    ajout commande hover

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["box"];
+
   connect() {
-    this.element.textContent = "Hello World!"
+    console.log("Hover controller connected");
+  }
+
+  async hover() {
+    const id = this.element.dataset.id;
+    const response = await fetch(`/commandes/${id}.json`);
+    const data = await response.json();
+    this.boxTarget.innerHTML = `
+      <p>Numero de commande: ${data.numero_de_commande}</p>
+    `;
+    this.boxTarget.style.display = "block";
+  }
+
+  unhover() {
+    this.boxTarget.style.display = "none";
   }
 }
